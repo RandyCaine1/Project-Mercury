5407b355f6a2785d43b8f30c473ce9a0bc22f21d
Hakeem Clarke <hakeem_clarke14@yahoo.com>
2018-05-17 21:08:45 +0000

    Run migrations

--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
 from . import db
 
-class UserProfile(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80))
-    last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(255))
+    email = db.Column(db.String(80))
 
     def is_authenticated(self):
         return True
--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from forms import LoginForm
-from models import UserProfile
+from models import User
 from app.models import *
 
 
